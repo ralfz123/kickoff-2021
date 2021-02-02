e557a2eed877b28ce0b6d3173bf69867285be0c8
ralfz123 <ralf_zonneveld@hotmail.com>
2021-02-02 13:02:29 +0100

    Added code to push data to mockuapi

--- js/app.js
+++ js/app.js
@@ -57,3 +57,91 @@ interestOneItem.innerHTML = putData.other.sport;
 interestTwoItem.innerHTML = putData.other.muziek;
 githubHandleItem.innerHTML = 'https://github.com/' + putData.githubHandle;
 githubHandleItem.href = 'https://github.com/' + putData.githubHandle;
+
+
+// ------
+
+// this code works
+// const path = `https://601803ee971d850017a3f625.mockapi.io/members`
+
+// async function main() {
+//   const me = await getData(path)
+
+//   putData(path + '/' + me[0].id, me[0])
+//     .then(data => {
+//       console.log('put', data);
+//     });
+
+//   // postData(path, temp)
+//   //   .then(data => {
+//   //     console.log('post', data);
+//   //   });
+
+
+//   // del(path + '/8')
+//   //   .then(data => {
+//   //     console.log('del', data);
+//   //   });
+// }
+
+// main()
+
+
+// async function getData(url) {
+//   return fetch(url)
+//     .then(response => response.json())
+//     .then(data => data.map(member => member.name == 'Ralf' ? {
+//       id: member.id,
+//       teamId: member.teamId,
+//       name: member.name,
+//       prefix: member.name,
+//       surname: member.surname,
+//       mugshot: 'https://avatars.githubusercontent.com/u/55750121?s=460&u=513e912aabf1c0f243452518e8dd740a31b5b164&v=4',
+//       githubHandle: 'ralfz123',
+//       other: {
+//         skills: {
+//           html: 4,
+//           css: 4,
+//           js: 2
+//         },
+//         bio: 'Ik ben een CMD-student en ik ben 21 jaar. Bovendien ben ik door de jaren heen echt gaan houden van HTML, CSS en JavaScript schrijven. Vakken als Project Tech en Tech-Track heb ik dan ook zeker gevolgd'
+//       }
+//     } : false).filter(item => typeof item === 'object'))
+
+// }
+
+
+// async function postData(url, data) {
+//   console.log(data)
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   });
+//   return response.json();
+// }
+
+// async function putData(url, data) {
+//   console.log(data)
+//   const response = await fetch(url, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   });
+//   return response.json();
+// }
+
+// async function del(url, data) {
+//   const response = await fetch(url, {
+//     method: 'DELELTE',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   });
+//   return response.json();
+// }
